fac2d77aff3627b9a2d3956c96f1d5dfe8a8ba9d
Hayley Witherell <hayleywitherell@gmail.com>
2023-08-30 10:08:19 -0600

    Finish tutorial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  formatted_view_count = view_count
+  rounded_count = view_count / 1000.0
+
+  if view_count >= 1000000
+    formatted_view_count = "#{(rounded_count / 1000).round(1)}M"
+  elsif view_count >= 1000
+    formatted_view_count = "#{rounded_count.round(1)}k"
+  end
+    formatted_view_count
 end
